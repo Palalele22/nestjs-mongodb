81858d456bf2c051edc5dac40ab24d56fa9dca0a
Pala <florin.palaghianu@student.usv.ro>
2023-10-06 11:54:32 +0300

    Fixed search & pagination

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -16,7 +16,7 @@ export class BookService {
   async findAll(query: Query): Promise<Book[]> {
     const resPerPage = 2;
     const currentPage = Number(query.page) || 1;
-    const skip = resPerPage * currentPage - 1;
+    const skip = resPerPage * (currentPage - 1);
 
     const keyword = query.keyword
       ? {
@@ -26,7 +26,11 @@ export class BookService {
           },
         }
       : {};
-    const books = await this.bookModel.find({ ...keyword }).limit(resPerPage).skip(skip);
+    const books = await this.bookModel
+      .find({ ...keyword })
+      .limit(resPerPage)
+      .skip(skip);
+    
     return books;
   }
 
